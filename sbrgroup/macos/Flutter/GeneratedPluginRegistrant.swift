e0f099e074bfecdc3845e87c8c244ea3e0038fef
KotipalliVenkatasaikumar <147800977+KotipalliVenkatasaikumar@users.noreply.github.com>
2024-10-23 18:29:13 +0530

    save

--- sbrgroup/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sbrgroup/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,8 +8,6 @@ import Foundation
 import connectivity_plus
 import device_info_plus
 import file_selector_macos
-import firebase_core
-import firebase_messaging
 import geolocator_apple
 import package_info_plus
 import path_provider_foundation
@@ -23,8 +21,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
